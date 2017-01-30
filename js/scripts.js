30f98f0f650f895224c7790adce08fd3a308bc99
Lawrence Eby and Renee Mei <lawrence@>
2017-01-30 08:58:22 -0800

    Add some for user inputed name

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 //business-logic
+var User = function(inputName){
+  this.name = inputName;
+}
+
 
 //front-end
 $(document).ready(function() {
-  alert("good morning, nerds!");
-}
+  var inputName = $("#user-name").val();
+  var newUser = new User(inputName);
+
+  $(".name-output").text(newUser.name);
+});
