6fb6e0d998baedd463b861fea72fccd491718535
Lawrence Eby and Renee Mei <lawrence@>
2017-01-30 09:01:35 -0800

    Add user input form listener

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,12 @@ var User = function(inputName){
 
 //front-end
 $(document).ready(function() {
-  var inputName = $("#user-name").val();
-  var newUser = new User(inputName);
+  $("#user-input").submit(function(event){
+    event.preventDefault();
+    
+    var inputName = $("#user-name").val();
+    var newUser = new User(inputName);
 
-  $(".name-output").text(newUser.name);
+    $(".name-output").text(newUser.name);
+  })
 });
