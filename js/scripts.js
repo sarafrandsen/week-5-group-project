331a3c9ba6f71bc7b5c5812e98b957ae6b0c1bde
Sara Frandsen <sara.frandsen@gmail.com>
2017-02-01 11:23:24 -0800

    edit content

--- js/scripts.js
+++ js/scripts.js
@@ -96,11 +96,11 @@ var userInputConversion = function(userObjectInput) {
     return "north";
   } else if (userObjectInput === "south") {
     return "south";
-  } else if (userObjectInput === "switch") {
+  } else if (userObjectInput === "emergency switch") {
     return "levelThreeButton";
   } else if (userObjectInput === "bunk") {
     return "bunk";
-  } else if (userObjectInput === "button") {
+  } else if (userObjectInput === "emergency button") {
     return "levelFourButton";
   } else if (userObjectInput === "carrots") {
     return "carrots";
