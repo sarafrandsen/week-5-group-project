252a3df2d3cc58eff8f595660c1aa33c7053110a
Keely Glenn and Sara Frandsen <sara@>
2017-02-01 11:17:58 -0800

    fix punctuation and variable names

--- js/levels.js
+++ js/levels.js
@@ -36,23 +36,23 @@ var Object = function(name, description, talk, pickup, go, fight, inter) {
 //level one
 var planet = new Object("planet", "The planet is terrestrial and looks a lot like your old home. You feel a little nostalgic.", "You sing a little love song to the planet.", "You cannot pickup the planet.", "level-up", "You fire your ship's laser at the planet. The laser vanishes once it hits the atmosphere.", "You take a selfie with the planet.");
 
-var radio = new Object("space radio", "Your radio only cost you a could of space bucks at the local space thrift store. What a deal!", "You sing along with the song on the space radio, just a little out of key.", "The space radio is heavier than it looks.", "You walk towards the space radio, but there is't anything interesting nearby.", "You think breaking your space radio is a bad idea.", "You turn the music up on your space radio.");
+var radio = new Object("space radio", "Your radio only cost you a couple of space bucks at the local space thrift store. What a deal!", "You sing along with the song on the space radio, just a little out of key.", "The space radio is heavier than it looks.", "You walk towards the space radio, but there is't anything interesting nearby.", "You think breaking your space radio is a bad idea.", "You turn the music up on your space radio.");
 
 //level two
-var north = new Object("north", "The pine trees look a little softer than the rocks", "You yell at the trees for not being a nice little clearing to land in", "You cannot pickup the trees", "double-level-up", "You fire lasers, and it burns the tops of the trees, but is otherwise ineffective", "You take a selfie with the trees, wasting valuable time.");
+var north = new Object("north", "The pine trees look a little softer than the rocks.", "You yell at the trees for not being a nice little clearing to land in.", "You cannot pickup the trees.", "double-level-up", "You fire lasers, and it burns the tops of the trees, but is otherwise ineffective.", "You take a selfie with the trees, wasting valuable time.");
 
-var south = new Object("south", "The rocks look really jagged and hard. Maybe the trees are a better decision", "You yell at the rocks for not being a clearing.", "You cannot pickup the rocks.", "level-up", "You shoot lasers at the rocks, and miss.", "You take a selfie with the rocks, wasting valuable time.");
+var south = new Object("south", "The rocks look really jagged and hard. Maybe the trees are a better decision.", "You yell at the rocks for not being a clearing.", "You cannot pickup the rocks.", "level-up", "You shoot lasers at the rocks, and miss.", "You take a selfie with the rocks, wasting valuable time.");
 
 //level three
 
-var levelThreeButton = new Object("switch", "This button will open the cockpit in any emergency", "You tell the button to open the cockpit, but you remember that you forgot to install voice control. Still on the to-do list...", "You try to pickup the button, but it's difficult and it doesn't work.", "You're already next to the button", "You try to smash the button, but you miss.", "double-level-up");
+var levelThreeButton = new Object("switch", "This button will open the cockpit in any emergency.", "You tell the button to open the cockpit, but you remember that you forgot to install voice control. Still on the to-do list...", "You try to pickup the button, but it's difficult and it doesn't work.", "You're already next to the button.", "You try to smash the button, but you miss.", "double-level-up");
 
 var bunk = new Object("bunk", "Your bed is a little lumpy, but the blankets are old and familiar, smelling a little like home.", "You tell your stuffed carrot that you love them.", "You try to pick up the bunk but it is bolted to the deck.", "You go to your bed to lie down but its not as comfortable as it looks.", "You throw all the blankets off the bed and onto the floor.", "You try to take a nap but the sense of adventure makes you restless.");
 
 // level four
-var levelFourButton = new Object("button", "This button will open the cockpit in any emergency", "You tell the button to open the cockpit, but you remember that you forgot to install voice control. Still on the to-do list...", "You try to pickup the button, but it's difficult and it doesn't work.", "You're already next to the button", "You try to smash the button, but you miss.", "level-up");
+var levelFourButton = new Object("button", "This button will open the cockpit in any emergency.", "You tell the button to open the cockpit, but you remember that you forgot to install voice control. Still on the to-do list...", "You try to pickup the button, but it's difficult and it doesn't work.", "You're already next to the button.", "You try to smash the button, but you miss.", "level-up");
 
-var carrots = new Object("carrots", "Your carrots look a little beat up from the crash but still edible.", "You tell your carrots how sorry you are that they got beat up.", "You pick up your carrots and put them in your pocket", "You walk towards your carrots and accidentally step on them.", "You throw your carrots at the wall and break them.", "add-to-inventory");
+var carrots = new Object("carrots", "Your carrots look a little beat up from the crash but still edible.", "You tell your carrots how sorry you are that they got beat up.", "You pick up your carrots and put them in your pocket.", "You walk towards your carrots and accidentally step on them.", "You throw your carrots at the wall and break them.", "add-to-inventory");
 
 // level five
 var turnip = new Object ("turnip", "The turnip is about a foot tall and a foot wide. The stalks on its head are styled into a very fashionable 'hair-do'. Its torso is a lovely shade of purple.", "The turnip asks again for your help." , "The turnip is indignant that you picked it up. This is wildly offensive in turnip culture.", "Don't move! There is a fox with a laser pointed at you.", "The turnip bites your foot and insists the fox is the bad guy.", "The turnip asks for your help again!");
@@ -64,10 +64,10 @@ var foxes = new Object("the foxes", "They seem to be talking about something you
 
 
 // level six
-var forest = new Object("forest", "The forest is dense with trees", "You whistle and it sounds like the forest whistles back.", "You pickup a stick from the ground and throw it deep into the forest.", "level-up", "You punch a tree and hurt your paw!", "You take a selfie with the forest, wasting valuable time.");
+var forest = new Object("forest", "The forest is dense with trees.", "You whistle and it sounds like the forest whistles back.", "You pickup a stick from the ground and throw it deep into the forest.", "level-up", "You punch a tree and hurt your paw!", "You take a selfie with the forest, wasting valuable time.");
 
 // level seven
-var quietly = new Object("sneak quietly", "You could sneak quietly if you decide to 'go' quietly", "You whisper to yourself about how sad it is that the foxes are eating turnips.", "You cannot pickup abstract things", "level-up", "You punch the air as a warm up.", "You take a selfie, quietly, with the camp behind you, wasting valuable time.");
+var quietly = new Object("sneak quietly", "You could sneak quietly if you decide to 'go' quietly.", "You whisper to yourself about how sad it is that the foxes are eating turnips.", "You cannot pickup abstract things.", "level-up", "You punch the air as a warm up.", "You take a selfie, quietly, with the camp behind you, wasting valuable time.");
 
 var foxes = new Object("fight the foxes", "They seem to be talking about something your can't quite hear from here.", "You can't talk to them from here, it would blow your cover!", "You cannot pickup these foxes.", "double-level-up", "double-level-up", "You take a selfie with the foxes, wasting valuable time.");
 
@@ -89,21 +89,21 @@ var rope = new Object("rope", "The rope is thick, but your sharp teeth are no ma
 // level eleven
 var microphone = new Object("microphone", "The microphone is attached to a long cord. It is very similar to a lasso or a ball and chain.", "Your voice is magnified to the crowd and startles you.", "fight3", "You go towards the microphone and knock it off the stand", "You hit the microphone and knock it off the stand", "level-up");
 
-var boilingWater = new Object("boiling water", "The pot of boiling water is large and black, there is steam rising from the top. It looks like it could badly burn your enemies.", "You tell the babies hanging about that you are there to rescue them.", "You try to pick up the pot but its too large and the hot water starts to overflow almost burning you", "You walk towards the boiling water but the steam makes it hard to see.", "You attack the pot of boiling water and it almost spills all over you!", "You reach towards the pot of boiling water and it almost spills all over you!");
+var boilingWater = new Object("boiling water", "The pot of boiling water is large and black, there is steam rising from the top. It looks like it could badly burn your enemies.", "You tell the babies hanging about that you are there to rescue them.", "You try to pick up the pot but its too large and the hot water starts to overflow almost burning you.", "You walk towards the boiling water but the steam makes it hard to see.", "You attack the pot of boiling water and it almost spills all over you!", "You reach towards the pot of boiling water and it almost spills all over you!");
 
-var leader = new Object("Le Leader du Les Foxes", "Le Leader du Les Foxes is wearing his beret proudly.", "You taunt him with bunny puns", "You can't pick him up at the moment.", "You're already pretty close.", "fight3", "You try to take a selfie with Le Leader du Les Foxes, but he stops you before you waste valuable time.");
+var leader = new Object("Le Leader du Les Foxes", "Le Leader du Les Foxes is wearing his beret proudly.", "You taunt him with bunny puns.", "You can't pick him up at the moment.", "You're already pretty close.", "fight3", "You try to take a selfie with Le Leader du Les Foxes, but he stops you before you waste valuable time.");
 
 // level twelve
 var zipper = new Object("zipper", "The zipper pull is hanging right below Le Leader's beret. The zipper spans the length of the space suit.", "Zippers cant talk you fool!", "What are you trying to do?", "level-up", "level-up", "level-up");
 
-var flee = new Object("flee", "Behind Le Leader du les Foxes is a clear path towards the forest, and behind you is a crowd of foxes.", "You won't be able to talk your way out of this situation!", "There is nothing in the area that might help you escape.", "You try to run, but a group of foxes block your path.", "You try fighting your way through, but there are too many foxes!", "You try running, but a group of foxes block your path!");
+var outside = new Object("outside", "Behind Le Leader du les Foxes is a clear path towards the forest, and behind you is a crowd of foxes.", "You won't be able to talk your way out of this situation!", "There is nothing in the area that might help you escape.", "You try to run, but a group of foxes block your path.", "You try fighting your way through, but there are too many foxes!", "You try running, but a group of foxes block your path. You take a selfie with them wasting valuable time!");
 
-var ship = new Object("ship", "The brand new ship modifications make it shine just like an KaratX90T!", "You pat your new ship and say, 'Good Karat...'", "You cannot lift your ship!", "game-end", "You don't want to fight this brand new ship", "game-end");
+var ship = new Object("ship", "The brand new ship modifications make it shine just like an KaratX90T!", "You pat your new ship and say, 'Good Karat...'", "You cannot lift your ship!", "game-end", "You don't want to fight this brand new ship.", "game-end");
 
 // level thirteen
 var foxJail = new Object("fox jail", "Fox jail is a prison established 300 years ago by the founders of Fox City. The crime rate in Fox City is quite low, so it is not highly populated. However, the leaders of Fox City ensure the best guards patrol the prison.", "level-up", "You cannot lift a giant prison. What are you trying to do?", "You need to return the baby turnips to their parents!", "The foxes are no longer the bad guys.", "level-up");
 
-var dinner = new Object("dinner", "The foxes don't look very hungry.", "You suggest they make turnip soup with Le Leader, but the foxes didn't find turnips appetizing to begin with.", "What are you trying to do?", "It's rude to invite yourself to someone else's meal.", "The foxes are now your friends", "The foxes don't look very hungry.");
+var pot = new Object("pot", "The foxes don't look very hungry.", "You suggest they make turnip soup with Le Leader, but the foxes didn't find turnips appetizing to begin with.", "What are you trying to do?", "It's rude to invite yourself to someone else's meal.", "The foxes are now your friends", "The foxes don't look very hungry.");
 
 
 
@@ -133,9 +133,9 @@ var level11 = new Level(11, "You're not known as Riley, Space Rabbit Extraordina
 
 var level12 = new Level(12, "Recalling your training in the art of Wushu, you flail the microphone like a ball and chain above your ears. It's time to take on the Le <span class='text-enemy'>[Leader]</span> du Les Foxes.");
 
-var level13 = new Level(13, "Le Leader du Les Foxes, ducks with a twirl and tries to kick your feet from under you. As they spin, you notice a <span class='text-object'>[zipper]</span> at the crown of their space helmet. You are also surrounded by more foxes than you can handle, and wonder if you should <span class='text-object'>[flee]</span> to regroup and fight the boss one-on-one another time.")
+var level13 = new Level(13, "Le Leader du Les Foxes, ducks with a twirl and tries to kick your feet from under you. As they spin, you notice a <span class='text-object'>[zipper]</span> at the crown of their space helmet. You are also surrounded by more foxes than you can handle, and wonder if you should flee <span class='text-object'>[outside]</span> to regroup and fight the boss one-on-one another time.")
 
-var level14 = new Level(14, "You, Riley, Space Rabbit Extraordinaire, use your powerful legs to jump behind Le Leader and tug the zipper down. Deflating, the space suit sinks to the legs of a--What is this? A turnip! A gasp resonates through the crowd. Confused, the foxes begin to disperse, muttering among themselves, 'we didn't even like turnips anyway' and 'we were only doing it because that guy said it was cool.' A few foxes, embarassed by the whole ordeal, hand you your trusty laser gun, the Unforgettable, (which you've totally forgotten about), ask you what they should do with the treachorous ex-leader of the FiSSwL. You have a couple ideas in mind. They could lock the turnip up in <span class='text-object'>[fox jail]</span>, they could save him for <span class='text-object'>[dinner]</span>.");
+var level14 = new Level(14, "You, Riley, Space Rabbit Extraordinaire, use your powerful legs to jump behind Le Leader and tug the zipper down. Deflating, the space suit sinks to the legs of a--What is this? A turnip! A gasp resonates through the crowd. Confused, the foxes begin to disperse, muttering among themselves, 'we didn't even like turnips anyway' and 'we were only doing it because that guy said it was cool.' A few foxes, embarassed by the whole ordeal, hand you your trusty laser gun, the Unforgettable, (which you've totally forgotten about), ask you what they should do with the treachorous ex-leader of the FiSSwL. You have a couple ideas in mind. They could lock the turnip up in <span class='text-object'>[fox jail]</span>, they could cook him for dinner in the <span class='text-object'>[pot]</span> of boiling water.");
 
 
 var level15 = new Level(15, "Carrying the rescued turnip babies, you make your way back to the turnip town. By now, the turnips have finished repairing your ship, and what was once a KaratX70T now sparkles like a KaratX90T. The foxes are gone, Le Leader is behind bars, the babies are back with their families, and you've got a sweet new ride. All-in-all, you'd call this adventure a successful one. It's time to say goodbye and take off in your <span class='text-object'>[ship]</span>, tucking the Unforgettable in its holster onto the co-pilot's seat.");
